4a594b32f86d423c60e5b5af1cad7257b8240c4e
avarenee <abiery@wesleyan.edu>
2019-09-13 20:08:54 -0400

    reformatted code for less repetition

---
+++ navbar.js
@@ -0,0 +1,13 @@
+var nav = `<div class="container">
+             <a href="index.html"><h1>Ava Biery</h1></a>
+             <nav>
+               <ul>
+                 <li><a href="about.html">About</a></li>
+                 <li><a href="projects.html">Projects</a></li>
+                 <li><a href="poetry.html">Poetry</a></li>
+                 <li><a href="contact.html">Contact</a></li>
+               </ul>
+             <nav>
+           </div>`
+
+document.getElementById("header").innerHTML = nav;
